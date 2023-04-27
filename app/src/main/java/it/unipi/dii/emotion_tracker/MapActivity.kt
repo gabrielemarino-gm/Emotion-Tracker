cfa83ca71ba28a8f99d16ed2e3ae6cb648f3e64b
matteorazzai1 <118298555+matteorazzai1@users.noreply.github.com>
2023-04-27 09:31:50 +0200

    deleted some commented code

--- app/src/main/java/it/unipi/dii/emotion_tracker/MapActivity.kt
+++ app/src/main/java/it/unipi/dii/emotion_tracker/MapActivity.kt
@@ -3,12 +3,13 @@ package it.unipi.dii.emotion_tracker
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.*
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat.getSystemService
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
@@ -49,13 +50,7 @@ class MapActivity: AppCompatActivity()
             &&
             ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 MY_PERMISSIONS_REQUEST_LOCATION)
@@ -69,27 +64,6 @@ class MapActivity: AppCompatActivity()
                 val latitude = location.latitude
                 val longitude = location.longitude
 
-                /*
-                val lat = location.latitude
-                val lon = location.longitude
-                val geoPoint = GeoPoint(lat, lon)
-                val geocoder = GeocoderNominatim(Locale.getDefault(), "MyUserAgent")
-                val addresses = geocoder.getFromLocation(geoPoint, 1)
-
-                if (addresses.isNotEmpty())
-                {
-                    val address = addresses[0]
-                    val street = address.thoroughfare
-                    val city = address.locality
-                    val country = address.countryName
-
-                    // Do something with the address information
-                }
-                else
-                {
-                    // No address found
-                }
-                */
 
                 val geocoder = Geocoder(applicationContext, Locale.getDefault())
                 val addresses = geocoder.getFromLocation(latitude, longitude, 1)
@@ -111,7 +85,7 @@ class MapActivity: AppCompatActivity()
                 marker.position = GeoPoint(latitude, longitude)
                 marker.title = "lat:${latitude}\n" +
                         "long:${longitude}\n" +
-                        " street:${street}\n" +
+                        "street:${street}\n" +
                         "city:${city}"
                 map.overlays.add(marker)
             }
