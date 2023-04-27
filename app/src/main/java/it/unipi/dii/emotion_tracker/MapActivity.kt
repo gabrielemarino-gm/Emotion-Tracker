5509e66ec129466308adb2f576c8c84930765fd5
matteorazzai1 <118298555+matteorazzai1@users.noreply.github.com>
2023-04-27 16:45:12 +0200

    Deleted some comments

--- app/src/main/java/it/unipi/dii/emotion_tracker/MapActivity.kt
+++ app/src/main/java/it/unipi/dii/emotion_tracker/MapActivity.kt
@@ -37,11 +37,11 @@ class MapActivity: AppCompatActivity()
 
         //myRef.setValue("Hello, World!")
 
-        println("firebase db")
+        //println("firebase db")
 
         var position_obtained: Int = 0
 
-        println("ciclo ${position_obtained}")
+        //println("ciclo ${position_obtained}")
 
 
         val map = findViewById<org.osmdroid.views.MapView>(R.id.map)
@@ -55,11 +55,7 @@ class MapActivity: AppCompatActivity()
         val startPoint = GeoPoint(41.8902, 12.4922)
         mapController.setCenter(startPoint)
 
-        /*val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            println("GPS provider is not enabled")
-        }*/
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
             &&
@@ -104,7 +100,7 @@ class MapActivity: AppCompatActivity()
 
                     position_obtained = 1
 
-                    println("scrittura ${position_obtained}")
+                    //println("scrittura ${position_obtained}")
 
                     val marker = Marker(map)
                     marker.position = GeoPoint(latitude, longitude)
@@ -126,92 +122,6 @@ class MapActivity: AppCompatActivity()
             Looper.getMainLooper()
         )
 
-
-        /*fusedLocationClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
-                // Got last known location. In some rare situations, this can be null.
-                if (location != null) {
-                    val latitude = location.latitude
-                    val longitude = location.longitude
-                    Log.d("TAG", "Latitude: $latitude, Longitude: $longitude")
-                    val geocoder = Geocoder(applicationContext, Locale.getDefault())
-                    val addresses = geocoder.getFromLocation(latitude, longitude, 1)
-                    val address = addresses?.get(0)
-
-                    val street = address?.thoroughfare
-                    val city = address?.locality
-
-
-                    // println("lat:${latitude}\nlong:${longitude}\nstreet:${street}\ncity:${city}")
-
-                    val location_cell=LocationCell(latitude,longitude,street,city)
-                    myRef.push().setValue(location_cell)
-
-                    val marker = Marker(map)
-                    marker.position = GeoPoint(latitude, longitude)
-                    marker.title = "lat:${latitude}\n" +
-                            "long:${longitude}\n" +
-                            "street:${street}\n" +
-                            "city:${city}"
-                    map.overlays.add(marker)
-
-                } else {
-                    Log.d("TAG", "No location found")
-                }
-            }
-            .addOnFailureListener { e ->
-                Log.d("TAG", "Error getting location: ${e.message}")
-            }*/
-
-        /*val locationListener = object : LocationListener
-        {
-            override fun onLocationChanged(location: Location)
-            {
-                val latitude = location.latitude
-                val longitude = location.longitude
-
-
-                val geocoder = Geocoder(applicationContext, Locale.getDefault())
-                val addresses = geocoder.getFromLocation(latitude, longitude, 1)
-                val address = addresses?.get(0)
-
-                val street = address?.thoroughfare
-                val city = address?.locality
-
-
-                // println("lat:${latitude}\nlong:${longitude}\nstreet:${street}\ncity:${city}")
-
-                val location_cell=LocationCell(latitude,longitude,street,city)
-                myRef.push().setValue(location_cell)
-
-                // delete the older mark into the map
-                val oldMarker = map.overlays.firstOrNull { it is Marker } as Marker?
-                if (oldMarker != null) {
-                    map.overlays.remove(oldMarker)
-                }
-
-                val marker = Marker(map)
-                marker.position = GeoPoint(latitude, longitude)
-                marker.title = "lat:${latitude}\n" +
-                        "long:${longitude}\n" +
-                        "street:${street}\n" +
-                        "city:${city}"
-                map.overlays.add(marker)
-            }
-
-            override fun onProviderDisabled(provider: String) {}
-
-            override fun onProviderEnabled(provider: String)
-            {
-                println("provider enabled function")
-            }
-
-            override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-        }
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
-
-        // locationManager.removeUpdates(locationListener)*/
     }
 }
 
