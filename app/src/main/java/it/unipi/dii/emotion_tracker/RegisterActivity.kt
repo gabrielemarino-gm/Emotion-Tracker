acaa8b890847f806d40aef04e77432545369dcd8
matteorazzai1 <118298555+matteorazzai1@users.noreply.github.com>
2023-05-01 18:27:25 +0200

    Added date of birth field to the registration page

--- app/src/main/java/it/unipi/dii/emotion_tracker/RegisterActivity.kt
+++ app/src/main/java/it/unipi/dii/emotion_tracker/RegisterActivity.kt
@@ -1,10 +1,10 @@
 package it.unipi.dii.emotion_tracker
 
 import android.content.Intent
+import android.icu.util.Calendar
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
 import java.nio.charset.StandardCharsets
@@ -24,6 +24,33 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val years = Array(currentYear - 1899) { i -> (i + 1900) }
+        val months = Array(12) { i -> (i + 1) }
+        val days = Array(31) { i -> (i + 1) }
+
+        val spinner_year = findViewById<Spinner>(R.id.spinner3)
+        val adapter_year = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, years)
+        spinner_year.adapter = adapter_year
+
+        val spinner_month = findViewById<Spinner>(R.id.spinner2)
+        val adapter_month = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, months)
+        spinner_month.adapter = adapter_month
+
+        val spinner_day = findViewById<Spinner>(R.id.spinner)
+        val adapter_day = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, days)
+        spinner_day.adapter = adapter_day
+
+        var selectedDay = findViewById<TextView>(R.id.testViewDay)
+        val selectedMonth = findViewById<TextView>(R.id.testViewMonth)
+        val selectedYear = findViewById<TextView>(R.id.testViewYear)
+        
+        setContentSpinner(selectedDay,spinner_day)
+        setContentSpinner(selectedMonth,spinner_month)
+        setContentSpinner(selectedYear,spinner_year)
+
+
+
         usernameText = findViewById(R.id.username_reg);
         passwordText = findViewById(R.id.password_reg);
         //dateOfBirth = findViewById(R.id.date_of_birth)
@@ -35,11 +62,17 @@ class RegisterActivity : AppCompatActivity() {
 
         registerButton.setOnClickListener(){
 
+            // TODO implement the date format if needed
+            //val calendar = Calendar.getInstance()
+            //calendar.set(selectedDay.text.toString().toInt(), selectedMonth.text.toString().toInt(), selectedYear.text.toString().toInt())
+            //val date_of_birth = calendar.time
+
             controlloUsername(usernameText.text.toString(),myRef) { controllo ->
                 if (controllo) {
                     val user = User(
                         usernameText.text.toString(),
-                        encryptPassword(passwordText.text.toString())
+                        encryptPassword(passwordText.text.toString()),
+                        "${selectedDay.text.toString().toInt()}/${selectedMonth.text.toString().toInt()}/${selectedYear.text.toString().toInt()}"
                     )
                     myRef.push().setValue(user)
 
@@ -61,6 +94,19 @@ class RegisterActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun setContentSpinner(selectedTextView: TextView, spinner: Spinner) {
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position)
+                    selectedTextView.text = selectedItem.toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //nothing to do
+            }
+        }
     }
 
     private fun controlloUsername(username: String, myRef: DatabaseReference, callback: (Boolean) -> Unit){
@@ -105,8 +151,8 @@ class RegisterActivity : AppCompatActivity() {
     }
 }
 
-class User(usernameText: String, passwordText:String) {
+class User(usernameText: String, passwordText: String, date_of_birth: String) {
     var username : String =usernameText
     var password : String =passwordText
-    //var date_of_birth : SimpleDateFormat =dateOfBirth
+    var date_of_birth : String = date_of_birth
 }
