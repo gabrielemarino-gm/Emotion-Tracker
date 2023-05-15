d231b98f3cf28012e0b5cca9ccfcf449dd1cc990
LijTesfaye <mesay1964@gmial.com>
2023-05-15 11:03:25 -0400

    HotFix on the MapActivity

--- app/src/main/java/it/unipi/dii/emotion_tracker/MapActivity.kt
+++ app/src/main/java/it/unipi/dii/emotion_tracker/MapActivity.kt
@@ -239,6 +239,29 @@ class MapActivity: AppCompatActivity()
 
                 //now we have also the filter on timestamp, and the results on the timeRefList
                 //println("listaquery-----------------------------------------")
+
+                for (element in longRefList) {
+                    val timeRef = element.value as HashMap<String, Any>
+
+                    println("DBG: element = $timeRef")
+
+                    val street = timeRef.get("street") as? String ?: "Unknown street"
+                    val city = timeRef.get("city") as? String ?: "Unknown city"
+
+                    val cluster = ClusterCentroid(
+                        timeRef.get("latitude") as Double,
+                        timeRef.get("longitude") as Double,
+                        street,
+                        city,
+                        timeRef.get("emotion") as Double,
+                        timeRef.get("numberOfPoints") as Long,
+                        timeRef.get("timestampDate") as Long
+                    )
+                    clusterList.add(cluster)
+                    timeRefs.add(timeRef)
+                }
+
+                /*
                 for (element in longRefList){
                     val timeRef=element.value as HashMap<String,Any>
 
@@ -255,6 +278,11 @@ class MapActivity: AppCompatActivity()
                     clusterList.add(cluster)
                     timeRefs.add(timeRef)
                 }
+                */
+
+
+
+
                 //println("size of list"+clusterList.size)
                 printMarkers(clusterList,map)
             }
